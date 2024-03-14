63521bfb7f901c31ef0cbc6e6e054fbb427e1b0a
Gurleen Kaur <gurleen.kaur@lpinfotech.com>
2024-03-14 17:26:03 +0530

    to-do list created

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-    $("#addToList").click(function(){
-        $("p").append($("#input-value").val() + "<br>");
+$(document).ready(function () {
+
+    function test() {
+        $(".btn-close").click(function () {
+            $(this).parent('li').addClass('hide');
+        });
+    };
+
+    test();
+
+    $("#addToList").click(function () {
+        var a = $("#input-value").val();
+        $("ul").append('<li>' + a + '<button type="button" class="btn-close ms-4" aria-label="Close"></button></li>')
+        test();
     });
-  });
+});
